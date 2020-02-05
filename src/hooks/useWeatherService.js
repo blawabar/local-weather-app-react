f335555486a7acc289f38c5b7f099e67f651db27
blawabar <fake.torrent@wp.pl>
2020-02-05 21:33:19 +0100

    add support for Geoloction related errors; add constant for holding fetch's URL

--- src/hooks/useWeatherService.js
+++ src/hooks/useWeatherService.js
@@ -1,20 +1,22 @@
 import { useEffect, useReducer } from "react";
 
-class FetchState {
+class ServiceState {
   static FETCH_INIT = 0;
   static FETCH_SUCCESS = 1;
   static FETCH_ERROR = 2;
+  static GEOLOCATION_ERROR = 3;
 }
 
 const reducerFunction = (state, action) => {
   const { type, payload } = action;
 
   switch (type) {
-    case FetchState.FETCH_INIT:
+    case ServiceState.FETCH_INIT:
       return { ...state, isLoading: true };
-    case FetchState.FETCH_SUCCESS:
+    case ServiceState.FETCH_SUCCESS:
       return { ...state, isLoading: false, weatherData: payload, error: null };
-    case FetchState.FETCH_ERROR:
+    case ServiceState.FETCH_ERROR:
+    case ServiceState.GEOLOCATION_ERROR:
       return { ...state, isLoading: false, weatherData: null, error: payload };
     default:
       return state;
@@ -44,22 +46,21 @@ const useWeatherService = () => {
     const signal = aborController.signal;
 
     const getData = async ({ lat, lon }) => {
+      const URL = `${API}?lat=${lat}&lon=${lon}&units=metric&APPID=${TOKEN}`;
+
       try {
-        dispatch({ type: FetchState.FETCH_INIT });
-        const result = await fetch(
-          `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&APPID=086370e96396a4464fe97ec16a0f7381`,
-          { signal }
-        );
+        dispatch({ type: ServiceState.FETCH_INIT });
+        const result = await fetch(URL, { signal });
 
         if (result.ok) {
           const data = await result.json();
-          dispatch({ type: FetchState.FETCH_SUCCESS, payload: data });
+          dispatch({ type: ServiceState.FETCH_SUCCESS, payload: data });
         } else {
           const error = await result.json();
-          dispatch({ type: FetchState.FETCH_ERROR, payload: error });
+          dispatch({ type: ServiceState.FETCH_ERROR, payload: error });
         }
       } catch (error) {
-        dispatch({ type: FetchState.FETCH_ERROR, payload: error });
+        dispatch({ type: ServiceState.FETCH_ERROR, payload: error });
       }
     };
 
@@ -85,22 +86,24 @@ const useWeatherService = () => {
     };
 
     const handleError = error => {
+      let msg = "An unknown Geolocation related error occurred.";
+
       switch (error.code) {
         case error.PERMISSION_DENIED:
-          alert("User denied the request for Geolocation.");
+          msg = "User denied the request for Geolocation.";
           break;
         case error.POSITION_UNAVAILABLE:
-          alert("Location information is unavailable.");
+          msg = "Location information is unavailable.";
           break;
         case error.TIMEOUT:
-          alert("The request to get user location timed out.");
+          msg = "The request to get user location timed out.";
           break;
         case error.UNKNOWN_ERROR:
-          alert("An unknown error occurred.");
-          break;
         default:
           break;
       }
+
+      dispatch({ type: ServiceState.GEOLOCATION_ERROR, payload: msg });
     };
 
     getCoords(handleGetCoords, handleError);
