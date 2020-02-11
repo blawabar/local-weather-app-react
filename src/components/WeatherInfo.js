29c6d443560cff36807209ad7eda750a86433db3
blawabar <fake.torrent@wp.pl>
2020-02-11 21:35:03 +0100

    move UnitsSwitch into a separate component; add some layout related tweaks

--- src/components/WeatherInfo.js
+++ src/components/WeatherInfo.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import "./WeatherInfo.scss";
 
+import UnitsSwitch from "./UnitsSwitch";
 import Location from "./Location";
 import Temperature from "./Temperature";
 import Description from "./Description";
@@ -15,40 +16,10 @@ const WeatherInfo = () => {
   const handleUnitsChange = () =>
     unitsType === "METRIC" ? setUnitsType("IMPERIAL") : setUnitsType("METRIC");
 
-  const UnitsSwitch = () => {
-    return (
-      <button
-        style={{
-          position: "absolute",
-          top: "15px",
-          left: "50%",
-          transform: "translateX(-50%)",
-          width: "75px",
-          margin: "0 auto",
-          padding: "0 10px",
-          background: "transparent",
-          border: "2px solid var(--secondary-text-color)",
-          borderRadius: "15px",
-          fontFamily: "Open Sans",
-          fontSize: "10px",
-          fontWeight: "700",
-          color: "var(--secondary-text-color)",
-          lineHeight: "1.75",
-          letterSpacing: "1px",
-          outline: "none",
-          cursor: "pointer"
-        }}
-        onClick={handleUnitsChange}
-      >
-        {unitsType}
-      </button>
-    );
-  };
-
   return (
     <div className="weather-info">
-      <UnitsSwitch />
       <UnitsContext.Provider value={unitsType}>
+        <UnitsSwitch handleUnitsChange={handleUnitsChange} />
         <Location />
         <Temperature />
         <Description />
