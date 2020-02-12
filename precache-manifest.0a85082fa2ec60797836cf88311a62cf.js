bf6851529d2b8956f8d76b11c97bc244b905abc5
blawabar <fake.torrent@wp.pl>
2020-02-12 21:46:11 +0100

    Updates

---
+++ precache-manifest.0a85082fa2ec60797836cf88311a62cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db144ea9d4dc5b050e183f2991af2ffe",
+    "url": "/local-weather-app-react/index.html"
+  },
+  {
+    "revision": "02282450d2df747e4b4e",
+    "url": "/local-weather-app-react/static/css/main.4bce372d.chunk.css"
+  },
+  {
+    "revision": "010edc3eb000a0a36bd5",
+    "url": "/local-weather-app-react/static/js/2.fa8ad33a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/local-weather-app-react/static/js/2.fa8ad33a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02282450d2df747e4b4e",
+    "url": "/local-weather-app-react/static/js/main.2595996f.chunk.js"
+  },
+  {
+    "revision": "d86069e1941beac6f516",
+    "url": "/local-weather-app-react/static/js/runtime-main.618ea4c1.js"
+  }
+]);
